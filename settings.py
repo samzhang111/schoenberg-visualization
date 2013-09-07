2c5b1536fcf67d38aefcfa80c4bea347a586f65e
JamesFW <james.wu@students.olin.edu>
2013-09-07 14:10:33 -0400

    Lib task to import all csv rows into mongoengine

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import os
+from mongoengine import *
+connect('sdbm')
 
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 TIMELINE_PATH = PROJECT_PATH + '/timeline'
@@ -15,7 +17,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.dummy', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -129,6 +131,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'timeline',
+
 )
 
 # A sample logging configuration. The only tangible logging
--- timeline/views.py
+++ timeline/views.py
@@ -2,18 +2,20 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from settings import STATIC_ROOT
 from settings import PROJECT_PATH
-from models import Transaction
+from timeline.models import Transaction
+
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "home.html"
-    model = Transaction
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(HomeView, self).get_context_data(**kwargs)
+        transactions = Transaction.objects.all
         # Add in a QuerySet of all the books
         context['staticstuff'] = STATIC_ROOT
         context['proj'] = PROJECT_PATH
+        context['transactions'] = transactions
 
         return context
