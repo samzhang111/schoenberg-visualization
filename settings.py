e28ad355625a2e42a65e9f3dbcd749f03fbafb5f
JamesFW <james.wu@students.olin.edu>
2013-09-07 10:17:03 -0400

    static d3

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 PROJECT_PATH = PROJECT_PATH + '/timeline/'
-MEDIA_ROOT = PROJECT_PATH + '/media/'
+MEDIA_ROOT = PROJECT_PATH + '/static/'
 # Django settings for sdbm project.
 
 DEBUG = True
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = MEDIA_ROOT
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
--- timeline/views.py
+++ timeline/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from settings import STATIC_ROOT
+from django.models import Transaction
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "home.html"
+    model = Transaction
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(HomeView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['STATIC_ROOT'] = STATIC_ROOT
+        return context
