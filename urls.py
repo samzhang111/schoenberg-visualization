b333e09cad9905b3212c7e57a52a6c13bbd96e4d
Amy Jin <amyjin@vpl094.apng.library.upenn.edu>
2013-09-07 11:14:13 -0400

    enabled admin ui;still need to set db engine

--- urls.py
+++ urls.py
@@ -3,8 +3,8 @@ from timeline.views import HomeView
 
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,6 +15,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^home/$', HomeView.as_view(), name='home')
 )
