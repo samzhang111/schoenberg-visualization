d72ff92d472d9c7a6c02a840afb891d026945f02
JamesFW <james.wu@students.olin.edu>
2013-09-07 20:12:27 -0400

    arcs and attempting to pprint contra.json

--- timeline/views.py
+++ timeline/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.core import serializers
 from settings import STATIC_ROOT
 from settings import PROJECT_PATH
 from timeline.models import Transaction
+import json
 
 
 # Create your views here.
@@ -12,10 +14,10 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(HomeView, self).get_context_data(**kwargs)
-        transactions = Transaction.objects.all
+        transactions = Transaction.objects.filter(manuscript_id__lte='500')
         # Add in a QuerySet of all the books
         context['staticstuff'] = STATIC_ROOT
         context['proj'] = PROJECT_PATH
-        context['transactions'] = transactions
+        context['transactions'] = serializers.serialize("json", transactions)
 
         return context
