ae099c43acacf2f42bf2fdaaba6a39f638c09925
JamesFW <james.wu@students.olin.edu>
2013-09-18 21:42:24 -0400

    Added view for root

--- timeline/views.py
+++ timeline/views.py
@@ -170,6 +170,14 @@ class ArcsView(TemplateView):
         return context
 
 
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
+
+
 class HomeView(TemplateView):
     template_name = "home.html"
 
